961759a643af71946d0f2c3015b9ce3e2fd39b39
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2022-10-10 20:38:05 -0500

    Operadores 2

---
+++ main.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    let form = document.querySelector("#Formulario");
+    form.addEventListener("submit", async(e)=>{
+        e.preventDefault();
+        let json = JSON.stringify(Object.fromEntries(new FormData(e.target)));
+        let config = {
+            method: form.method,
+            body: json
+        };
+        let peticion = await fetch(form.action, config);
+        let data = await peticion.text();
+        document.querySelector("pre").innerHTML = data;
+    })
+})
